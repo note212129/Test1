6da03fcfeca2d56283f502505e4e32471dd15fc3
S0907 <changch_shtu@shuztung.com.tw>
2014-10-08 16:22:21 +0800

    1.0.1
    Add: SpeedButton 更改為Tag觸發

--- Unit1.pas
+++ Unit1.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, Menus, IniFiles, StdCtrls, ComCtrls,Unit4, FileCtrl, ExtCtrls,
+  Dialogs, Menus, IniFiles, StdCtrls, ComCtrls, Unit4, FileCtrl, ExtCtrls,
   Buttons, XPMan;
 
 type
@@ -33,9 +33,7 @@ type
 
     procedure Timer1Timer(Sender: TObject);
     procedure Edit2Change(Sender: TObject);
-    procedure SpeedButton1Click(Sender: TObject);
-    procedure SpeedButton2Click(Sender: TObject);
-    procedure SpeedButton3Click(Sender: TObject);
+    procedure SpeedButtonClick(Sender: TObject);
 
     procedure FileListBox1MouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
@@ -51,7 +49,7 @@ type
 
 var
   Form1: TForm1;
-  filedir:string;
+  filedir: string;
 
 implementation
 
@@ -59,44 +57,45 @@ implementation
 
 procedure TForm1.mloadClick(Sender: TObject);
 begin
-    FileLoad;
+  FileLoad;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-   DriveComboBox1.DirList:=DirectoryListBox1;
-   DirectoryListBox1.FileList:=FileListBox1;
-   FileListBox1.FileEdit:=Edit2;
-   FileListBox1.Mask:='*.doc';
+  DriveComboBox1.DirList := DirectoryListBox1;
+  DirectoryListBox1.FileList := FileListBox1;
+  FileListBox1.FileEdit := Edit2;
+  FileListBox1.Mask := '*.doc';
 
-   Edit1.Text:='';
-   Edit2.Text:='';
-   RichEdit1.Text:='';
+  Edit1.Text := '';
+  Edit2.Text := '';
+  RichEdit1.Text := '';
 
-   mload.Caption:='&load';    //快捷鍵設定
-   msave.Caption:='s&ave';
-   file1.Caption:='&file';
+  mload.Caption := '&load'; //快捷鍵設定
+  msave.Caption := 's&ave';
+  file1.Caption := '&file';
 
 //   Form1.Caption:=OpenDialog1.InitialDir;   //初始路徑
 end;
 
 procedure TForm1.msaveClick(Sender: TObject);
 var
-  FileExe:string;
-  mlenght:Integer;
+  FileExe: string;
+  mlenght: Integer;
 begin
-  SaveDialog1.Title:='Please enter File Name only~';
+  SaveDialog1.Title := 'Please enter File Name only~';
   SaveDialog1.Execute;
-  mlenght:=Length(SaveDialog1.FileName);
-  FileExe:=Copy(SaveDialog1.FileName,mlenght-3,4);
-  if CompareText(FileExe,'.doc')=0 then
-      RichEdit1.Lines.SaveToFile(SaveDialog1.FileName)
+  mlenght := Length(SaveDialog1.FileName);
+  FileExe := Copy(SaveDialog1.FileName, mlenght - 3, 4);
+  if CompareText(FileExe, '.doc') = 0 then
+    RichEdit1.Lines.SaveToFile(SaveDialog1.FileName)
   else
-  RichEdit1.Lines.SaveToFile(SaveDialog1.FileName+'.doc');
+    RichEdit1.Lines.SaveToFile(SaveDialog1.FileName + '.doc');
 end;
+
 procedure TForm1.mExitClick(Sender: TObject);
 begin
-   close;
+  close;
 end;
 
 procedure TForm1.Timer1Timer(Sender: TObject);
@@ -106,59 +105,56 @@ procedure TForm1.Timer1Timer(Sender: TObject);
 begin
   {  DecodeTime(Now,hour,min,sec,msec);
     msec_str:=IntToStr(msec);  }
-    if RichEdit1.Lines.Text ='' then
-    begin
-       msave.Enabled:=False;
-    end
-    else
-        msave.Enabled:=True;
-    Form1.Caption:=DateToStr(Date)+timetostr(Now);//+'.'+msec_str;
+  if RichEdit1.Lines.Text = '' then
+  begin
+    msave.Enabled := False;
+  end
+  else
+    msave.Enabled := True;
+  Form1.Caption := DateToStr(Date) + timetostr(Now); //+'.'+msec_str;
 end;
 
 procedure TForm1.Edit2Change(Sender: TObject);
 begin
-  Edit1.Text:=FileListBox1.FileName;
-  if FileListBox1.FileName<>''  then
+  Edit1.Text := FileListBox1.FileName;
+  if FileListBox1.FileName <> '' then
     RichEdit1.Lines.LoadFromFile(FileListBox1.FileName);
 end;
 
-procedure TForm1.SpeedButton1Click(Sender: TObject);
+procedure TForm1.SpeedButtonClick(Sender: TObject);
+var I: integer;
 begin
-    RichEdit1.Font.Color:=clRed;
-end;
-
-procedure TForm1.SpeedButton2Click(Sender: TObject);
-begin
-    RichEdit1.Font.Color:=clGreen;
-end;
-
-procedure TForm1.SpeedButton3Click(Sender: TObject);
-begin
-  RichEdit1.Font.Color:=clBlue;
+  I:=TControl(Sender).Tag;
+  case I of
+    1: RichEdit1.Font.Color := clRed;
+    2: RichEdit1.Font.Color := clGreen;
+    3: RichEdit1.Font.Color := clBlue;
+  end;
 end;
 
 procedure TForm1.FileListBox1MouseDown(Sender: TObject;
   Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
 begin
-   RichEdit1.Lines.LoadFromFile(FileListBox1.FileName);
+  RichEdit1.Lines.LoadFromFile(FileListBox1.FileName);
 end;
 
 procedure TForm1.FileLoad;
 begin
-  if   OpenDialog1.Execute=True then
+  if OpenDialog1.Execute = True then
   begin
-    filedir:=OpenDialog1.FileName;
-    Edit1.Text:=filedir;
-    Form1.Caption:=filedir;
+    filedir := OpenDialog1.FileName;
+    Edit1.Text := filedir;
+    Form1.Caption := filedir;
     RichEdit1.Lines.LoadFromFile(filedir);
     Form4.Show;
   end;
-end;  
+end;
 
 procedure TForm1.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
 begin
-    if MessageDlg('leave?',mtConfirmation,[mbYes,mbNo],1)=mryes then
-      Exit
-    else ;
+  if MessageDlg('leave?', mtConfirmation, [mbYes, mbNo], 1) = mryes then
+    Exit
+  else ;
 end;
 end.
+
